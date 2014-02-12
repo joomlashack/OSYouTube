8d392a089065f26b193b58e209f7f6b87da1c78d
Bill Tomczak <bill@ostraining.com>
2014-02-12 15:19:34 -0800

    Remove test file

--- src/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package   plg_content_youtubeembed
- * @contact   www.ostraining.com, support@ostraining.com
- * @copyright 2013 Open Source Training, LLC. All rights reserved
- * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
- */
-
-defined('_JEXEC') or die();
