f2823676797c5ae36aec474103fa07577fd984ed
Bill Tomczak <bill@ostraining.com>
2014-03-28 14:39:52 -0700

    Update copyright notices

--- src/youtubeembed.php
+++ src/youtubeembed.php
@@ -2,7 +2,7 @@
 /**
  * @package   plg_content_youtubeembed
  * @contact   www.ostraining.com, support@ostraining.com
- * @copyright 2013 Open Source Training, LLC. All rights reserved
+ * @copyright 2013-2014 Open Source Training, LLC. All rights reserved
  * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
 
