bf663bce09cf5d58abd29b269633729a04f95417
Anderson Grüdtner Martins <anderson@ostraining.com>
2014-09-22 15:18:50 -0300

    Fix package name on the file comment header

--- src/osyoutube.php
+++ src/osyoutube.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package   plg_content_osyoutube
+ * @package   OSYouTube
  * @contact   www.ostraining.com, support@ostraining.com
  * @copyright 2013-2014 Open Source Training, LLC. All rights reserved
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
