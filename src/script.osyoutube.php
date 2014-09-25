eb871de3cdd0625dff1246e6a12888736928a802
Anderson Grüdtner Martins <anderson@ostraining.com>
2014-09-24 22:37:56 -0300

    Add installer script and default views for it
    Set publish=true

---
+++ src/script.osyoutube.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package   OSYouTube
+ * @contact   www.ostraining.com, support@ostraining.com
+ * @copyright 2013-2014 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+require 'installer/include.php';
+
+defined('_JEXEC') or die();
+
+class PlgContentOSYouTubeInstallerScript extends AllediaInstallerAbstract
+{
+}
