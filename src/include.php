b0d782428c4c4ace97bd12b65d4aedec2d9c752f
Bill Tomczak <bill@joomlashack.com>
2025-12-11 08:37:59 -0800

    Update file headers

--- src/include.php
+++ src/include.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
  * @package   OSYouTube
  * @contact   www.joomlashack.com, help@joomlashack.com
- * @copyright 2021-2024 Joomlashack.com. All rights reserved
+ * @copyright 2021-2026 Joomlashack.com. All rights reserved
  * @license   https://www.gnu.org/licenses/gpl.html GNU/GPL
  *
  * This file is part of OSYouTube.
--- src/library/Free/Methods.php
+++ src/library/Free/Methods.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
  * @package   OSYouTube
  * @contact   www.joomlashack.com, help@joomlashack.com
- * @copyright 2017-2024 Joomlashack.com. All rights reserved
+ * @copyright 2017-2026 Joomlashack.com. All rights reserved
  * @license   https://www.gnu.org/licenses/gpl.html GNU/GPL
  *
  * This file is part of OSYouTube.
